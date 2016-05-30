25ce1ea9760675c24b60851e224c5bc4dbf3e6e7
dj-tech <www.giuseppe@dellavedova.org>
2016-05-30 19:48:05 +0200

    Create web.py

---
+++ web.py
@@ -0,0 +1,12 @@
+import django
+django.setup ()
+#questa zona è ancora da sviluppare
+import hat
+import os
+import sys
+os.syatem
+open
+import serial
+arduino.open ()
+arduino.close()
+#fine della parte ancora da sviluppare
